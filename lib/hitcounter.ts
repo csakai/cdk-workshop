ac6ddcdf80ae99114b46f8efe14aac5e8d3b8f83
Chris Sakai <chris@meetfabric.com>
2021-04-15 17:52:48 -0400

    Bind table and lambda to HitCounter

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
   /** the func in which we want to count url hits **/
@@ -7,8 +8,22 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends cdk.Construct {
+  public readonly handler: lambda.Function;
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'hitcounter.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
   }
 }
