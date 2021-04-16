d676ce12550961ed92d087d779c02d7bb1ad5596
Chris Sakai <chris@meetfabric.com>
2021-04-15 20:01:02 -0400

    Add table viewer

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -21,6 +22,11 @@ export class CdkWorkshopStack extends cdk.Stack {
     // defines APIGW REST API resource backed by hello fn
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: // ??????
     });
   }
 }
